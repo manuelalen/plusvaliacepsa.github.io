5d319249d426b2ea1c113eb0481235b8f5ba1a11
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2023-03-10 20:32:43 +0100

    Update app.R

--- ahorro/app.R
+++ ahorro/app.R
@@ -6,6 +6,8 @@ ui <- fluidPage(
   textInput("nombre", "Nombre:"),
   # Agrega un campo de texto para ingresar la edad
   numericInput("ahorro", "Ahorro:", value = NULL),
+  # Agrega un campo de texto para ingresar el mes
+  textInput("mes", "Mes:"),
   # Agrega un botón para guardar los datos en un archivo CSV
   actionButton("guardar", "Guardar datos"),
   # Agrega un mensaje para confirmar que se han guardado los datos
@@ -15,9 +17,9 @@ ui <- fluidPage(
 # Define el servidor
 server <- function(input, output) {
   # Crea una función para guardar los datos
-  guardar_datos <- function(nombre, ahorro) {
+  guardar_datos <- function(nombre, ahorro, mes) {
     # Crea un data frame con los datos
-    datos <- data.frame(Nombre = nombre, Ahorro = ahorro)
+    datos <- data.frame(Nombre = nombre, Ahorro = ahorro, Mes = mes)
     # Guarda los datos en un archivo CSV
     write.csv(datos, "datos.csv", row.names = FALSE, append = TRUE)
   }
@@ -25,7 +27,7 @@ server <- function(input, output) {
   # Agrega un observador para el botón de guardar
   observeEvent(input$guardar, {
     # Llama a la función para guardar los datos
-    guardar_datos(input$nombre, input$ahorro)
+    guardar_datos(input$nombre, input$ahorro, input$mes)
     # Muestra un mensaje de confirmación
     output$mensaje <- renderPrint("Los datos se han guardado correctamente.")
   })
