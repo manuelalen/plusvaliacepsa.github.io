1a000465f1cbf6ca377f54921e2d0bdb50303742
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2023-12-07 17:39:24 +0100

    Create creacion_tablas.sql

---
+++ Bancoobrero/creacion_tablas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE dev_testeos.bancoobrero(
+									 DNI VARCHAR(99),
+                                     NOMBRECOMPLETO VARCHAR(99),
+                                     N_CUENTA VARCHAR(99),
+                                     CREDITOS FLOAT,
+                                     NIVEL CHAR);
